6137e7bf50e1f289121261ee0113e5917f1cad23
Shaar <shaar@MacBook-Pro-2.local>
2022-04-15 13:42:45 -0400

    First commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const userInput = window.prompt("Choose to Deposit or Withdraw");
+let depositAmount;
+let withdrawAmount;
+let balance = 0;
+
+
+if (userInput === "Deposit" || userInput === "deposit") {
+    depositAmount = window.prompt("How much would you like to deposit?");
+    balance = Number(balance) + Number(depositAmount)
+    alert(`Your new balance is: $${balance}`)
+} else if (userInput === "Withdraw" || userInput === "withdraw") {
+    withdrawAmount = window.prompt("How much would you like to withdraw?");
+    balance = Number(balance) - Number(withdraw)
+    alert(`Your new balance is: $${balance}`)
+} else {
+    alert("Your input was invalid")
+}
