b617da73af7659642d925a6f11fcb4a9dbe58472
Shaar <shaar@MacBook-Pro-2.local>
2022-04-15 13:57:14 -0400

    quick attempt at question 3

--- main.js
+++ main.js
@@ -15,3 +15,21 @@ if (userInput === "Deposit" || userInput === "deposit") {
 } else {
     alert("Your input was invalid")
 }
+
+
+// My uncomplete attempt at question 3 below...
+/*
+for (let rep = 1; rep <= 10; rep ++) {
+    if (userInput === "Deposit" || userInput === "deposit") {
+        depositAmount = window.prompt("How much would you like to deposit?");
+        balance = Number(balance) + Number(depositAmount)
+        alert(`Your new balance is: $${balance}`)
+    } else if (userInput === "Withdraw" || userInput === "withdraw") {
+        withdrawAmount = window.prompt("How much would you like to withdraw?");
+        balance = Number(balance) - Number(withdraw)
+        alert(`Your new balance is: $${balance}`)
+    } else {
+        alert("Your input was invalid")
+    }
+}
+*/
